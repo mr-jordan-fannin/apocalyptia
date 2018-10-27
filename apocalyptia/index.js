e9a642e5d6f9028f1a1d1b156e6a58775c3fccc9
Jordan Fannin <jordan.fannin@kofile.us>
2018-10-26 23:59:17 -0400

    local http routing

---
+++ apocalyptia/index.js
@@ -0,0 +1,10 @@
+function loadFile(file, target) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', file, true);
+    xhr.onreadystatechange = function() {
+        if (this.readyState!==4) return;
+        if (this.status!==200) return;
+        document.getElementById(target).innerHTML= this.responseText;
+    };
+    xhr.send();
+}
